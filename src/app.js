4375291f6241112b19f1b2ccc46c4f0d9cb474bd
mudegiarnold <136020298+mudegiarnold@users.noreply.github.com>
2023-12-11 04:36:51 +0000

    Added Templates and Dynamic Pages

--- src/app.js
+++ src/app.js
@@ -1,13 +1,77 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.set('views', `${__dirname}/pages`);
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs: JOBS, companyName: "Jovian"});
 })
+
+app.get('/jobs/:id', (req, res) => {
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    res.render('job', { job: matchedJob});
+})
+
+const transporter = nodemailer.createTransport({
+    host: 'amudegi@gmail.com', //SMPT host
+    port: 3000, //SMPT port
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_ID,
+      pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+app.post('/jobs/:id/apply', (req, res) => {
+    const { name, email, phone, dob, coverletter } = req.body;
+
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+  
+    console.log('req.body', req.body);
+    console.log('matchedJob', matchedJob)
+
+    const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: process.env.EMAIL_ID,
+        subject: `New Application for ${matchedJob.title}`,
+        html: `
+          <p><strong>Name:</strong> ${name}</p>
+          <p><strong>Email:</strong> ${email}</p>
+          <p><strong>Phone:</strong> ${phone}</p>
+          <p><strong>Date of Birth:</strong> ${dob}</p>
+          <p><strong>Cover Letter:</strong> ${coverletter}</p>
+        `
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send('Error sending email');
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200).send('Email sent successfully');
+        }
+      });
+
+});
+
 
 const port = process.env.PORT || 3000;
 
